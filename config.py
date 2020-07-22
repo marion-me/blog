0605230d1438dce0b506e22034554edc2eca0458
marion vutagwa <marionvutagwa@yahoo.com>
2020-07-22 22:06:31 +0300

    add all new changes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    #SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://marion:pabo@localhost/blogging'
+    #SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://marion:pabo@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     QUOTE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
 
@@ -24,7 +24,7 @@ class ProdConfig(Config):
        
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://marion:pabo@localhost/blogging'
+    SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://marion:pabo@localhost/blog'
     DEBUG = True
 
 Config_options = {
